7a07cd189e0d53a3927b2bb8f54db5a67d8d78a7
Ushma Yadav <ushma.yadav@nearbuy.com>
2020-08-23 15:32:20 +0530

    Updates

---
+++ precache-manifest.55a438e15ad3668dcef08fa732f606fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9870ef99896e6135be7458d3a421c98",
+    "url": "/transfer-component/index.html"
+  },
+  {
+    "revision": "735a0168f2542b2840d0",
+    "url": "/transfer-component/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40ea55af25a823882de0",
+    "url": "/transfer-component/static/js/2.b99a7edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/transfer-component/static/js/2.b99a7edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735a0168f2542b2840d0",
+    "url": "/transfer-component/static/js/main.049f04ed.chunk.js"
+  },
+  {
+    "revision": "284b1eb0c61c42bd7f34",
+    "url": "/transfer-component/static/js/runtime-main.b3e12536.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/transfer-component/static/media/logo.5d5d9eef.svg"
+  }
+]);
